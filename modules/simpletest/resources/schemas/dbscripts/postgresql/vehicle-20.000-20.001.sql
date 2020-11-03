96d07e51cf0488c829c7264c2a61a392714bc801
labkey-tchad <tchad@labkey.com>
2020-11-02 17:06:23 -0800

    Add db table for testing ontologies

---
+++ modules/simpletest/resources/schemas/dbscripts/postgresql/vehicle-20.000-20.001.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE vehicle.otest
+(
+    RowId SERIAL NOT NULL,
+    reported VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    label VARCHAR(255) NOT NULL
+);
---
+++ modules/simpletest/resources/schemas/dbscripts/sqlserver/vehicle-20.000-20.001.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE vehicle.otest
+(
+    RowId INT IDENTITY(1,1),
+    reported NVARCHAR(255) NOT NULL,
+    code NVARCHAR(255) NOT NULL,
+    label NVARCHAR(255) NOT NULL
+);
